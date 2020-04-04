97184d52363d2ce00632775af6447f6fe4bad085
bitglicher <34365609+bitglicher@users.noreply.github.com>
2020-04-03 20:04:51 -0600

    Add files via upload

--- arch/d16i.c
+++ arch/d16i.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-//#include "d16i.h"
+#include "inset.h"
 #include "alu_ops.h"
 #include "../libs/arg_table.h"
 #include "../libs/bin_buffer.h"
@@ -7,52 +7,53 @@
 #include "../internals_backend.h"
 #include "d16i.h"
 
+/*ALU Instructions*/
+INS_NODE_TEMPLATE add = {.op = iADD, .name="add", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+INS_NODE_TEMPLATE sub = {.op = iSUB, .name="sub", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+INS_NODE_TEMPLATE and = {.op = iAND, .name="and", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+INS_NODE_TEMPLATE xor = {.op = iXOR, .name="xor", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+INS_NODE_TEMPLATE or = {.op = iOR, .name="or", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+INS_NODE_TEMPLATE not = {.op = iNOT, .name="not", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+INS_NODE_TEMPLATE shftl = {.op = iSHFTL, .name="shftl", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+INS_NODE_TEMPLATE shftr = {.op = iSHFTR, .name="shftr", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
 
-INS_NODE_TEMPLATE add = {.op = 0x00, .name="add", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
-INS_NODE_TEMPLATE sub = {.op = 0x01, .name="sub", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
-INS_NODE_TEMPLATE and = {.op = 0x02, .name="and", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
-INS_NODE_TEMPLATE xor = {.op = 0x03, .name="xor", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
-INS_NODE_TEMPLATE or = {.op = 0x04, .name="or", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
-INS_NODE_TEMPLATE not = {.op = 0x05, .name="not", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
-INS_NODE_TEMPLATE shftl = {.op = 0x06, .name="shftl", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
-INS_NODE_TEMPLATE shftr = {.op = 0x07, .name="shftr", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_alu};
+/*LOAD IMMEDIATE*/
+INS_NODE_TEMPLATE loadi = {.op =  0x00, .name="load", .nargs = 3, .relative_args = {0, 0, 0}, .asm_func = &assemble_loadi};
 
+/*Example to override a definition*/
+INS_NODE_TEMPLATE loadi_assisted = {.op =  0x01, .name="load", .nargs = 2, .relative_args = {0, 0}, .asm_func = &assemble_loadi};
 
+void assemble_loadi(BIN_BUFFER* bin_buffer, ARG_TABLE* arg_table, int op)
+{
+        switch (op)
+        {
+            case 0:
+                printf("\t\t\t\tload immediate detected\n");
+			    printf("\t\t\t\tADDR: ENCODED: %x\n", ENCODE_LI(arg_table->data[0].value, arg_table->data[1].value, arg_table->data[2].value, arg_table->data[2].value));
+    			printf("\t\t\t\tARG 0: %d\n", arg_table->data[0].value);
+    			printf("\t\t\t\tARG 1: %d\n", arg_table->data[1].value);
+    			printf("\t\t\t\tARG 2: %d\n", arg_table->data[2].value);
+    			printf("\t\t\t\tARG 3: %d\n", arg_table->data[3].value);
+            break;
+            case 1:
+                printf("\t\t\t\tload assisted detected\n");
+			    printf("\t\t\t\tADDR: ENCODED: %x\n", ENCODE_LI(arg_table->data[0].value, arg_table->data[1].value, arg_table->data[2].value, arg_table->data[2].value));
+    			printf("\t\t\t\tARG 0: %d\n", arg_table->data[0].value);
+    			printf("\t\t\t\tARG 1: %d\n", arg_table->data[1].value);
+    			printf("\t\t\t\tARG 2: %d\n", arg_table->data[2].value);
+    			printf("\t\t\t\tARG 3: %d\n", arg_table->data[3].value);
+			break;
 
+            default:
+            break;
+        }
+        /*The load assisted is a function that combines two loadi instructions*/
+}
 
 void assemble_alu(BIN_BUFFER* bin_buffer, ARG_TABLE* arg_table, int op)
 {
-        printf("\t\t\t\tADDR: ENCODED: %x\n", ENCODE_ALUF(op, arg_table->data[0].value, arg_table->data[1].value, arg_table->data[2].value));
-        printf("\t\t\t\tARG 0: %d\n", arg_table->data[0].value);
-        printf("\t\t\t\tARG 1: %d\n", arg_table->data[1].value);
-        printf("\t\t\t\tARG 2: %d\n", arg_table->data[2].value);
-    /*switch (op)
-    {
-    case op_type_add:
-        //append_byte(bin_buffer, match_args())
-    break;
-    case op_type_sub:
-
-    break;
-    case op_type_and:
-
-    break;
-    case op_type_xor:
-
-    break;
-    case op_type_or:
-
-    break;
-    case op_type_not:
-
-    break;
-    case op_type_shftl:
-
-    break;
-    case op_type_shftr:
-
-    break;
-    default:
-        break;
-    }*/
+    printf("\t\t\t\tADDR: ENCODED: %x\n", ENCODE_ALUF(op, arg_table->data[0].value, arg_table->data[1].value, arg_table->data[2].value));
+    printf("\t\t\t\tARG 0: %d\n", arg_table->data[0].value);
+    printf("\t\t\t\tARG 1: %d\n", arg_table->data[1].value);
+    printf("\t\t\t\tARG 2: %d\n", arg_table->data[2].value);
 }
--- arch/d16i.h
+++ arch/d16i.h
@@ -9,15 +9,19 @@
 
 /*Instruction encoding*/
 
-#define ENCODE_NOOP 0x0000
-#define ENCODE_LI(hl, gs, rt, imm) ((0x01) | (hl << 3) | (gs << 4) | (rt << 5) | (imm << 8))
-#define ENCODE_LSINS(ls, gs1, gs2, t1, p1) ((0x02) | (ls << 3) | (gs1 << 4) | (gs2 << 5) | (t1 << 6) | (p1 << 9))
+#define ENCODE_NOOP (unsigned short)0x0000
+#define ENCODE_LI(hl, gs, rt, imm) (unsigned short)((0x01) | (hl << 3) | (gs << 4) | (rt << 5) | (imm << 8))
+#define ENCODE_LSINS(ls, gs1, gs2, t1, p1) (unsigned short)((0x02) | (ls << 3) | (gs1 << 4) | (gs2 << 5) | (t1 << 6) | (p1 << 9))
 #define ENCODE_ALUF(op, rt, s1, s2) (unsigned short)((0x03) | (op << 3) | (rt << 7) | (s1 << 10) | (s2 << 13))
-#define ENCODE_INMI(op, imm) ((0x04) | (op << 3) | (ivr << 8))
-#define ENCODE_JMPR(ivr, op, ap, t1, t2) ((0x05) | (ivr << 3) | (op << 4) | (ap << 7) | (t1 << 10) | (t2 << 13))
-#define ENCODE_MOVF(hl_t, hl_s, ft, op, rt, s1) ((0x06) | (hl_t << 3) | (hl_s << 4) | (ft << 5) | (op << 6) | (rt << 10) | (s1 << 13))
-#define ENCODE_JMPFR(ivr, op, ap, t1, t2) ((0x07) | (ivr << 3) | (op << 4) | (ap << 7) | (t1 << 10) | (t2 << 13))
+#define ENCODE_INMI(op, imm) (unsigned short)((0x04) | (op << 3) | (ivr << 8))
+#define ENCODE_JMPR(ivr, op, ap, t1, t2) (unsigned short)((0x05) | (ivr << 3) | (op << 4) | (ap << 7) | (t1 << 10) | (t2 << 13))
+#define ENCODE_MOVF(hl_t, hl_s, ft, op, rt, s1) (unsigned short)((0x06) | (hl_t << 3) | (hl_s << 4) | (ft << 5) | (op << 6) | (rt << 10) | (s1 << 13))
+#define ENCODE_JMPFR(ivr, op, ap, t1, t2) (unsigned short)((0x07) | (ivr << 3) | (op << 4) | (ap << 7) | (t1 << 10) | (t2 << 13))
 
+//D16i Classess
+#define D16i_CLASS_GPR  0
+#define D16i_CLASS_SPR  1
+#define D16i_CLASS_SEC  2
 
 #define REGS_NAMES \
     /*GPRS*/ \
@@ -30,13 +34,33 @@
      4, 5, 6, 7,\
      0, 1, 2}
 
+/*To diferentiate different types of registers*/
+#define REG_CLASS \
+    {0, 0, 0, 0,\
+     0, 0, 0, 0,\
+     1, 1, 1}
+
 //including addr 0 because the backend subtracts 1 to the value
 #define NREGS 11
+
+
+#define TARGET_KEYWORDS \
+    {"hi", "lo" \
+    }
+
+#define TARGET_KEYWORDS_VALS \
+    {1, 0 \
+    };
+
+#define N_TARGET_KEYWORDS 2
+
+#define TARGET_KEYWORDS_TYPES \
+    {0, 0}
 
 /*Function prototypes must, have sometype of contex on runtime*/
 /*op and name to give context, ARG_TABLE to assemble and bin buffer*/
 void assemble_alu(BIN_BUFFER* bin_buffer, ARG_TABLE* arg_table, int op);
-
+void assemble_loadi(BIN_BUFFER* bin_buffer, ARG_TABLE* arg_table, int op);
 
 //ALU operations
 extern INS_NODE_TEMPLATE add;
@@ -47,7 +71,8 @@ extern INS_NODE_TEMPLATE or;
 extern INS_NODE_TEMPLATE not;
 extern INS_NODE_TEMPLATE shftl;
 extern INS_NODE_TEMPLATE shftr;
-
+extern INS_NODE_TEMPLATE loadi;
+extern INS_NODE_TEMPLATE loadi_assisted;
 
 
 #define INS_TEMPLATE_ARRAY \
@@ -60,12 +85,17 @@ extern INS_NODE_TEMPLATE shftr;
         &not,  \
         &shftl,\
         &shftr,\
+        &loadi,\
+        &loadi_assisted\
     }
 
-#define N_INS_TEMPLATES 8
+#define N_INS_TEMPLATES 10
 
 void assembler_func();
 
 #define ASSMBLER_FUNCTION(INS) assembler_func(INS)
+
+//Memory Addresses per instruction
+#define NADDR_INS 1 
 
 #endif
