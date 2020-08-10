f6b88afbc75f99b2bda596b4dc90655b46a112b5
bitglitcher <caminlobo@gmail.com>
2020-08-10 08:27:32 -0500

    helper macros added to target description

---
+++ arg_access.h
@@ -0,0 +1,16 @@
+/*
+ * Autor: Benjamin Herrera Navarro
+ * 8/10/2020 7:12 AM
+ * This file provides helper macros for better access of argument nodes
+ */
+
+
+#define GET_OP1 arg_table->data[0]
+#define GET_OP2 arg_table->data[1]
+#define GET_OP3 arg_table->data[2]
+#define GET_OP4 arg_table->data[3]
+#define GET_OP5 arg_table->data[4]
+
+#define REGISTER "register"
+#define NUMERIC "numeric"
+#define ADDRESS "address"
