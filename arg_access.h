c77986fb04d54e8c16ad51743bf812b30b46443e
bitglitcher <caminlobo@gmail.com>
2020-08-20 16:11:50 -0500

    more macros added for quick access of arguments

--- arg_access.h
+++ arg_access.h
@@ -11,6 +11,24 @@
 #define GET_OP4 arg_table->data[3]
 #define GET_OP5 arg_table->data[4]
 
+#define GET_OP1_VAL arg_table->data[0].value
+#define GET_OP2_VAL arg_table->data[1].value
+#define GET_OP3_VAL arg_table->data[2].value
+#define GET_OP4_VAL arg_table->data[3].value
+#define GET_OP5_VAL arg_table->data[4].value
+
+#define GET_OP1_NAME arg_table->data[0].name
+#define GET_OP2_NAME arg_table->data[1].name
+#define GET_OP3_NAME arg_table->data[2].name
+#define GET_OP4_NAME arg_table->data[3].name
+#define GET_OP5_NAME arg_table->data[4].name
+
+#define GET_OP1_NAME arg_table->data[0].domain
+#define GET_OP2_NAME arg_table->data[1].domain
+#define GET_OP3_NAME arg_table->data[2].domain
+#define GET_OP4_NAME arg_table->data[3].domain
+#define GET_OP5_NAME arg_table->data[4].domain
+
 #define REGISTER "register"
 #define NUMERIC "numeric"
 #define ADDRESS "address"
