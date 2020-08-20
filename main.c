8680181ec769f3132db4c94851ca42c8caa70d4b
bitglitcher <caminlobo@gmail.com>
2020-08-20 18:15:24 -0500

    Support for raw output

--- main.c
+++ main.c
@@ -21,6 +21,7 @@ static char args_doc[] = "[FILENAME]...";
 
 static struct argp_option options[] = { 
     { "output", 'o', "FILE", 0, "Place output file as <file>."},
+    { "raw", 'r', 0, 0, "Output raw bytes."},
     { "table", 't', 0, 0, "Display symbol table."},
     { 0 } 
 };
@@ -29,6 +30,7 @@ struct arguments {
     char* output_file;
     char* input_file;
     bool table_display;
+    bool raw;
 };
 
 static error_t parse_opt(int key, char *arg, struct argp_state *state) {
@@ -36,6 +38,7 @@ static error_t parse_opt(int key, char *arg, struct argp_state *state) {
     switch (key) {
         case 'o': arguments->output_file = strdup(arg); break;
         case 't': arguments->table_display = true; break;
+        case 'r': arguments->raw = true; break;
         case ARGP_KEY_ARG: arguments->input_file = strdup(arg); return 0;
         default: return ARGP_ERR_UNKNOWN;
     }   
@@ -86,6 +89,7 @@ int main(int argc, char *argv[])
     arguments.output_file = NULL;
     arguments.input_file = NULL;
     arguments.table_display = false;
+    arguments.raw = false;
 
     argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
@@ -94,8 +98,16 @@ int main(int argc, char *argv[])
         printf("Input File %s\n", arguments.input_file);
         if(arguments.output_file == NULL)
         {
-            printf("Using default output -> ROM.bin\n");
-            arguments.output_file = "obj.o";
+            if(arguments.raw)
+            {
+                arguments.output_file = "ROM.bin";
+            }
+            else
+            {
+                printf("Using default output -> ROM.bin\n");
+                arguments.output_file = "obj.o";
+            }
+            
         }
         else
         {
@@ -144,17 +156,43 @@ int main(int argc, char *argv[])
                 printf("0x%x ", bin_buffer.data[i]); 
                 col_cnt++;
             }
-            printf("\n");
-            //Transfer bin_buffer to the object file structure
-            OBJ_FILE obj; 
-            obj.size = bin_buffer.size + 1;
-            obj.binary_data = malloc(sizeof(char) * obj.size);
-            obj.reloc_table.reloc = malloc(sizeof(RELOC) * 1);
-            obj.reloc_table.size = 1;
-            obj.reloc_table.reloc[0].name = "hola";
-            obj.reloc_table.reloc[0].size = strlen(obj.reloc_table.reloc[0].name);
-            memcpy(obj.binary_data, bin_buffer.data, obj.size);
-            serialize(arguments.output_file, &obj);
+            if(arguments.raw)
+            {
+                FILE* raw_output = fopen(arguments.output_file, "w+");
+                if(raw_output == NULL)
+                {
+                    printf("dasm: " ANSI_COLOR_RED "fatal error" ANSI_COLOR_RESET ": creating object file\n");
+                    return -1; 
+                }
+                else
+                {
+                    if(raw_output == NULL)
+                    {
+                        printf("dasm: " ANSI_COLOR_RED "fatal error" ANSI_COLOR_RESET ": creating object file\n");
+                        return -1; 
+                    }
+                    else
+                    {   
+                        //Write file
+                        fwrite(bin_buffer.data, sizeof(char), bin_buffer.size, raw_output);
+                    }
+                }
+            }
+            else
+            {
+                printf("\n");
+                //Transfer bin_buffer to the object file structure
+                OBJ_FILE obj; 
+                obj.size = bin_buffer.size + 1;
+                obj.binary_data = malloc(sizeof(char) * obj.size);
+                obj.reloc_table.reloc = malloc(sizeof(RELOC) * 1);
+                obj.reloc_table.size = 1;
+                obj.reloc_table.reloc[0].name = "hola";
+                obj.reloc_table.reloc[0].size = strlen(obj.reloc_table.reloc[0].name);
+                memcpy(obj.binary_data, bin_buffer.data, obj.size);
+                serialize(arguments.output_file, &obj);
+            }
+            
         }
         else
         {
