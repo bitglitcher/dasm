ba442a4598c2b20a60ef691b6fec918bc20d7907
Benjamin Herrera Navarro <caminlobo@gmail.com>
2021-02-06 03:23:58 -0600

    preprocessor function to support macros added but no used

--- preprocessor/preprocessor.c
+++ preprocessor/preprocessor.c
@@ -2,6 +2,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include "preprocessor.h"
 
 
 //Return Modified Buffer
@@ -143,6 +144,145 @@ char *cnvrt_schar(char *str, size_t size)
 		}
 	}
 	return strdup(copy);
+}
+
+//Should only be called once to initialize the preprocessor define table
+void init_preprocessor(preprocessor_table_t* preprocessor_table)
+{
+	preprocessor_table->size = -1; //Currently the size is set to 0, meaning that this buffer is empty
+	preprocessor_table->define_entry = malloc(sizeof(preprocessor_table->define_entry) * preprocessor_table->size);
+	preprocessor_table->capacity = INIT_ENTRY_TABLE_SIZE;
+}
+
+void append_define(preprocessor_table_t* preprocessor_table,  char* name, char* contents)
+{
+	if(preprocessor_table->capacity < preprocessor_table->size)
+	{
+		preprocessor_table->capacity *= 2; //Double capacity
+		preprocessor_table->define_entry = realloc(preprocessor_table->define_entry, preprocessor_table->size * sizeof(define_entry_t));
+	}
+	preprocessor_table->size++;
+	preprocessor_table->define_entry[preprocessor_table->size].name = name;
+	preprocessor_table->define_entry[preprocessor_table->size].contents = contents;
+}
+
+define_entry_t* search_define(preprocessor_table_t* preprocessor_table,  char* name)
+{
+	for(int i = 0;i <= preprocessor_table->size;i++)
+	{
+		if(strcmp(preprocessor_table->define_entry[i].name, name) == 0) //Check if fies are the same
+		{
+			return &preprocessor_table->define_entry[i];
+		}
+	}
+	//If no occurrence was found, just return null
+	return NULL;
+}
+
+//If the string is found to be a whole identifier, and the it matches the
+//search string, then it will be replaced by the string argument.
+char* replace_string(char* buffer, char* string, char* search)
+{
+    //Legal character that are allowed as prefix or postfix leading characters
+    char allowed [] = ",.!@#$/` ^&*()?><~+=[]{}:;\n";
+    
+    //Save old buffer
+    char* copy = strdup(buffer);
+    char* modified_buffer = strdup(buffer);
+    //Find search match
+    char* ptr = strstr(modified_buffer, search);
+    int offset = (ptr - modified_buffer);
+    while(ptr)
+    {
+        if(ptr)
+    	{
+    		//Try to match the whole string
+    		if(memcmp(ptr, search, strlen(search)) == 0)
+    		{
+    			//Check posfix and prefix
+    		    char* substring = search;
+                bool prefix_legal = false;
+                bool postfix_legal = false;
+                for(int i = 0;i < strlen(allowed);i++)
+                {
+                    printf("%d %c %d\n", ptr, *ptr, modified_buffer);
+                    if(ptr > modified_buffer)
+                    {
+                        if(*(ptr - 1) == allowed[i])
+                        {
+                            prefix_legal = true;
+                        }
+                    }
+                    else
+                    {
+                        prefix_legal = true;
+                    }
+                    if((ptr + strlen(substring)) < modified_buffer)
+                    {
+                        if(*(ptr + strlen(substring) + 1) == allowed[i] || *(ptr + strlen(substring) + 1) == '\0')
+                        {
+                            postfix_legal = true;
+                        }
+                    }
+                    else
+                    {
+                        postfix_legal = true;
+                    }
+                }
+                if(prefix_legal && postfix_legal)
+    			{
+    				if(!modified_buffer)
+    				{
+    					modified_buffer = realloc(modified_buffer, sizeof(buffer) * (strlen(string) + strlen(buffer)));
+    				}
+    				else if((sizeof(modified_buffer) * (strlen(string) + strlen(modified_buffer))) > strlen(modified_buffer))
+    				{
+    					modified_buffer = realloc(modified_buffer, sizeof(buffer) * (strlen(string) + strlen(buffer)));
+    				}
+    				//printf("modified_buffer before: %s\n", modified_buffer);
+    				//Proceed to replace the string
+                    memcpy(modified_buffer, copy, strlen(string) + offset);
+    				memcpy(modified_buffer + offset, string, strlen(string));
+    				memcpy((modified_buffer + offset + strlen(string)), (copy + offset + strlen(substring)), strlen(copy) - (offset + strlen(substring)));
+    	            ptr += strlen(search);
+    				//printf("modified_buffer after: %s\n", modified_buffer);
+    			}
+    			else
+    			{
+                    ptr += strlen(search);
+    			}
+    		}
+    	}
+    	ptr = strstr(ptr, search);
+    	offset = (ptr - modified_buffer);
+    	memcpy(copy, modified_buffer, strlen(modified_buffer));
+    }
+    return modified_buffer;
+}
+
+void preprocess_memory(preprocessor_table_t* preprocessor_table, char* memory, size_t size)
+{
+	//Replace all defines with their respective deine_entry content
+	for(int i = 0;i <= preprocessor_table->size;i++)
+	{
+		//Search first character occurence
+		char* ptr = memchr(memory, *preprocessor_table->define_entry[i].name, size);
+		if(ptr) //Check for a valid pointer
+		{
+			//Then try to compare string
+			//Check if there are still memory bytes left, so it doesn't go into a unallocated memory region
+			if((ptr-memory) <= size)
+			{
+				if(memcmp(memory, preprocessor_table->define_entry->name, strlen(preprocessor_table->define_entry[i].name)) == 0) //If contents are equal
+				{
+					//Then replace with the contents of the define entry
+					char* copy = malloc(sizeof(memory) * (size + strlen(preprocessor_table->define_entry[i].name)));
+					memcpy(copy, memory, size);
+				}
+			}
+		}
+	}
+
 }
 
 //int main()
--- preprocessor/preprocessor.h
+++ preprocessor/preprocessor.h
@@ -1,7 +1,35 @@
+#ifndef __PREPROCESSOR_H__
+#define __PREPROCESSOR_H__
+
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
+#define INIT_ENTRY_TABLE_SIZE 0xff
+
+typedef struct
+{
+    char* name; //Define indentifier
+    char* contents;
+} define_entry_t;
+
+typedef struct
+{
+    size_t size; //Always points to the last added entry index.
+    size_t capacity;
+    define_entry_t* define_entry;
+} preprocessor_table_t;
 
 extern char* remove_line_comment(char *str, size_t size);
 extern char* remove_block_comment(char *str, size_t size);
 extern char* remove_gnu_as_comment(char* str, size_t size);
+
+extern void init_preprocessor(preprocessor_table_t* preprocessor_table);
+extern void append_define(preprocessor_table_t* preprocessor_table,  char* name, char* contents);
+extern define_entry_t* search_define(preprocessor_table_t* preprocessor_table,  char* name);
+extern char* replace_string(char* dest, char* string, char* search);
+extern void preprocess_memory(preprocessor_table_t* preprocessor_table, char* memory, size_t size);
+extern char* replace_defines(char* memory, size_t size);
+
+#endif
